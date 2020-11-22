291aa68887998da928e826368274de4b22cbebdd
Sumit Mahanag <sumit.en1997@gmail.com>
2020-11-22 17:40:21 +0530

    git api fuction compleate

--- js/github_stats_api.js
+++ js/github_stats_api.js
@@ -8,6 +8,7 @@ fetch('https://api.github.com/users/roodymike/repos')
     .then((data) => {
         var repo_count = Object.keys(data).length;
         var language_count = 0;
+        var reposize = 0;
         var language_array = [];
         for (i = 0; i < repo_count; i++) {
             if ((data[i]).language != null && !(language_array.includes((data[i]).language, 0))) {
@@ -16,12 +17,15 @@ fetch('https://api.github.com/users/roodymike/repos')
             }
             //console.log((data[i].language));
         }
-        console.log(language_array);
-        console.log(language_count);
-        //var language_count = Object.keys(data.language);
+        for (i = 0; i < repo_count; i++) {
+            reposize += parseInt(data[i].size);
+        }
+        //console.log(language_array);
+        //console.log(language_count);
+        //console.log(reposize / 1000);
+        document.getElementById('totalreposize').innerHTML = (reposize / 1000).toFixed(2);
         document.getElementById('repo_count').innerHTML = repo_count;
         document.getElementById('language_count').innerHTML = language_count;
-
         //console.log(Object.keys(data).length);
 
     });
